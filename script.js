2c58190bf7551fe224f9e9bb3c6e7ffc347c4e63
madiinemm <uraimovamadina179@gmail.com>
2024-01-02 16:06:58 +0500

    todoapp has been created

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");   
+
+function validate(){
+    if (!inputBox.value) {
+        alert('You must write something❗');
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    inputBox.value = '';
+    saveData()
+}
+listContainer.addEventListener('click', function (e) {
+    if(e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveData()
+    } else if(e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveData()
+    }
+})
+
+btn.addEventListener("click", function () {
+    if (validate()){
+
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+function show() {
+    listContainer.innerHTML = localStorage.getItem('data');
+} show();
+
+
+
+
